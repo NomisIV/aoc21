2e1aa7042d6d72e76d93bcb096ca55e79d56be45
NomisIV <simon@nomisiv.com>
2021-12-07 19:14:10 +0100

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Advent of Code 2021";
+
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs;
+  };
+
+  outputs = { self, nixpkgs }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in {
+    devShell.x86_64-linux = pkgs.mkShell {
+      buildInputs = with pkgs; [ rustc cargo ];
+    };
+  };
+}
