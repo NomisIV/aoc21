489a9e11393642a5b5fdef188e8b3aa38544485d
NomisIV <simon@nomisiv.com>
2021-12-07 20:00:22 +0100

    Add part 2

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,12 +1,33 @@
 use std::io::{self, BufRead};
 
 fn main() -> io::Result<()> {
+
+    let input: Vec<i32> = io::stdin().lock().lines().map(|line| {
+        line.unwrap().parse().unwrap()
+    }).collect();
+
     let mut prev = -1;
     let mut x = -1;
+    for num in input.clone() {
+        if num > prev {
+            x += 1;
+        }
+        prev = num;
+    }
 
-    for line in io::stdin().lock().lines() {
-        let line = line?;
-        let num = line.parse::<i32>().unwrap();
+    println!("{}", x);
+
+    let avg_input: Vec<i32> = {
+        let mut vec = Vec::new();
+        for n in 1..(input.len() - 1) {
+            vec.push(input[n-1] + input[n] + input[n+1]);
+        };
+        vec
+    };
+
+    let mut prev = -1;
+    let mut x = -1;
+    for num in avg_input {
         if num > prev {
             x += 1;
         }
